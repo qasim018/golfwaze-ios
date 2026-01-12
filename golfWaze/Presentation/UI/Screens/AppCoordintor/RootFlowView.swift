e61c6c257f7046fdb44d2ac2b5169101e2c29cb9
Muhammad Qasim <muhammad.qasim@evampsaanga.com>
2026-01-12 16:15:54 +0500

    inital file setup.

--- golfWaze/Presentation/UI/Screens/AppCoordintor/RootFlowView.swift
+++ golfWaze/Presentation/UI/Screens/AppCoordintor/RootFlowView.swift
@@ -43,6 +43,11 @@ struct RootFlowView: View {
         .onAppear {
             authCoordinator.moveToTabbar = {
                 appCoordinator.moveToTabbar()
+                
+                // for testing purpose only
+                
+                
+                
             }
         }
     }
